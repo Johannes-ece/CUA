1f323736a517837c800cbf0dd2a1a5e389645c7c
Johannes von Grundherr <74217475+Johannes-ece@users.noreply.github.com>
2024-01-20 03:53:19 +0100

    new

--- packages/app/provider/auth/index.web.tsx
+++
@@ -1,6 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  //@ts-ignore
-  return <ClerkProvider>{children}</ClerkProvider>;
-}
